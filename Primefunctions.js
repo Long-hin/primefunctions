805c191605f14135486720c9743e5a4f8c084b51
Logan <logan.tan@wsu.edu>
2020-11-02 20:30:45 -0800

    Finished max PrimeSum.

--- Primefunctions.js
+++ Primefunctions.js
@@ -2,13 +2,14 @@
 // Fundamentals HW
 // eslint-disable-next-line no-unused-vars
 function PrimeGen(input) {
-  const list = Array.from({ length: input / 2 }, (_, i) => 2 * i + 1);
+  const arraylen = Math.ceil(input / 2);
+  const list = Array.from({ length: arraylen }, (_, i) => 2 * i + 1);
   list[0] = 2;
-  const accepted = Array(input / 2).fill(true);
+  const accepted = Array(arraylen).fill(true);
   const primes = [];
   let StopValue = _.max(list);
   StopValue = Math.sqrt(StopValue);
-  StopValue = Math.trunc(StopValue);
+  StopValue = Math.ceil(StopValue);
   for (let i = 0; list[i] < StopValue; i++) {
     if (accepted[i]) {
       for (let e = 0; e < list.length; e++) {
@@ -26,28 +27,34 @@ function PrimeGen(input) {
   return primes;
 }
 function CumulativeSum(input) {
-  const WorkSet = input;
+  const WorkSet = Array.from(input);
   for (let i = 1; i < input.length; i++) {
     WorkSet[i] += WorkSet[i - 1];
   }
   return WorkSet;
 }
 function maxPrimeSum(input) {
+  let tuple = [0, 0];
+  const length = [];
+  let loc = 0;
   const list = PrimeGen(input);
-  const sums = CumulativeSum(list);
-  const set = Array(2);
-  let test = 0;
-  for (let i = list.length; i >= 0; i--) {
-    test = _.find(list, function (thing) { if (sums[i] === thing) return true; return false; });
-    console.log(list);
-    if (test !== undefined) {
-      set[0] = test;
-      set[1] = i;
-      break;
+  let sums = CumulativeSum(list);
+  const items = [];
+  for (let a = 0; a < list.length; a++) {
+    for (let i = 0; i < list.length; i++) {
+      if (_.contains(sums, list[i])) {
+        loc = _.indexOf(sums, list[i]) + 1;
+        length.push(loc);
+        items.push(list[i]);
+      }
     }
+    list.shift();
+    sums = CumulativeSum(list);
   }
-  return set;
+  loc = _.indexOf(length, _.max(length));
+  tuple = [items[loc], _.max(length)];
+  return tuple;
 }
-console.log(PrimeGen(20));
+console.log(PrimeGen(963));
 console.log(CumulativeSum(PrimeGen(20)));
 console.log(maxPrimeSum(1000));
