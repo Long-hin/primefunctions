da162aebd60adf10c067cb5c29c53ac34c5503f6
Logan <logan.tan@wsu.edu>
2020-12-08 21:17:47 -0800

    Test cases have been created for Testing correctness, showing an issue with the case when 10 is the input to primeGen

---
+++ testCorrectness.js
@@ -0,0 +1,30 @@
+describe('PrimeGen(10)',function(){
+  it ('must equal [2,3,5,7]', function(){
+    chai.expect(PrimeGen(10)).to.deep.equal([2,3,5,7]);
+  });
+});
+describe('PrimeGen(20)',function(){
+  it ('must equal [2,3,5,7,11,13,17,19]',function() {
+    chai.expect(PrimeGen(20)).to.deep.equal([2,3,5,7,11,13,17,19]);
+  });
+});
+describe('cumulativeSum([1,2,3,4])',function(){
+  it ('must equal [1,3,6,10]', function(){
+    chai.expect(CumulativeSum([1,2,3,4])).to.deep.equal([1,3,6,10]);
+  });
+});
+describe('cumulativeSum([10,11,12,13,14])',function(){
+  it ('must equal [10,21,33,46,60]',function() {
+    chai.expect(CumulativeSum([10,11,12,13,14])).to.deep.equal([10,21,33,46,60]);
+  });
+});
+describe('maxPrimeSum(100)',function(){
+  it ('must equal [41, 6]', function(){
+    chai.expect(CumulativeSum([1,2,3,4])).to.deep.equal([1,3,6,10]);
+  });
+});
+describe('maxPrimeSum(1000)',function(){
+  it ('must equal [953, 21]',function() {
+    chai.expect(CumulativeSum([10,11,12,13,14])).to.deep.equal([10,21,33,46,60]);
+  });
+});
